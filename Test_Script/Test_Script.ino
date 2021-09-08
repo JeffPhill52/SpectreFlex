4990fa2aac99f1e6c24f57351db8078e1b0cd665
Jeffrey Phillips <jspf73@mst.edu>
2021-09-08 11:24:28 -0500

    added buttons

--- Test_Script/Test_Script.ino
+++ Test_Script/Test_Script.ino
@@ -1,8 +1,13 @@
+// Libraries for Bloothtooth Module
+#include <SoftwareSerial.h>
 
 // Libraries for Accelerometer
 #include <Adafruit_MPU6050.h>
 #include <Adafruit_Sensor.h>
 #include <Wire.h>
+
+// Bluetooth Initialization and Constants
+SoftwareSerial BTserial(2, 3); // RX | TX
 
 // Accelerometer Initialization and Constants
 Adafruit_MPU6050 mpu;
@@ -11,13 +16,24 @@ void setup() {
   Serial.begin(9600);
   Serial.println("Arduino Started");
 
-  // put your setup code here, to run once:
+  // output LEDs
   pinMode(3, OUTPUT);
   pinMode(4, OUTPUT);
   pinMode(5, OUTPUT);
   pinMode(6, OUTPUT);
   pinMode(7, OUTPUT);
   pinMode(8, OUTPUT);
+
+  // input tactile buttons
+  pinMode(9, INPUT);
+  pinMode(10, INPUT);
+  pinMode(11, INPUT);
+  pinMode(12, INPUT);
+
+    /* 
+   *  Bluetooth Setup
+   */
+  BTserial.begin(9600);
 
   /*
    * Accelerometer Setup
@@ -55,12 +71,37 @@ void loop() {
   //   }
   // }
 
-  /* Get new sensor events with the readings */
-  sensors_event_t a, g, temp;
-  mpu.getEvent(&a, &g, &temp);
+  // /* Get new sensor events with the readings */
+  // sensors_event_t a, g, temp;
+  // mpu.getEvent(&a, &g, &temp);
 
-  //Serial.println("Accel: " + String(a.acceleration.x) + " " + String(a.acceleration.y) + " " + String(a.acceleration.z));
-  Serial.println("Gyro: " + String(g.gyro.x) + " " + String(g.gyro.y) + " " + String(g.gyro.z));
-  delay(100);
+  // //Serial.println("Accel: " + String(a.acceleration.x) + " " + String(a.acceleration.y) + " " + String(a.acceleration.z));
+  // Serial.println("Gyro: " + String(g.gyro.x) + " " + String(g.gyro.y) + " " + String(g.gyro.z));
+  // delay(100);
+
+  if(digitalRead(9) == HIGH)
+  {
+    digitalWrite(8, HIGH);
+  }
+  else
+  {
+    digitalWrite(8, LOW);
+  }
+  if(digitalRead(10) == HIGH)
+  {
+    digitalWrite(7, HIGH);
+  }
+  else
+  {
+    digitalWrite(7, LOW);
+  }
+  if(digitalRead(11) == HIGH)
+  {
+    digitalWrite(6, HIGH);
+  }
+  else
+  {
+    digitalWrite(6, LOW);
+  }
 
 }
