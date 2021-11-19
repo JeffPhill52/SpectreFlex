319c0db5ec9085790f43da5539867540642a93ec
Jeffrey Phillips <jspf73@mst.edu>
2021-11-19 09:37:00 -0600

    added flex sensor outputs

--- Hardware_Test/Hardware_Test.ino
+++ Hardware_Test/Hardware_Test.ino
@@ -37,33 +37,42 @@ void setup() {
 
 void loop() {
 
-//  // test BLED
-//  bool led_on = millis() - flash_time >= 1000;
-//  bool reset_led = millis() - flash_time >= 2000;
-//  // if 1 second has passed and light is currently off
-//  if (led_on)
-//  {
-//    digitalWrite(BLED, HIGH);
-//    if (!message_sent)
-//    {
-//      Serial.println("BLED on");
-//      message_sent = true;
-//    }
-//  }
-//  // if less than 1 second and light is on
-//  if (reset_led)
-//  {
-//    digitalWrite(BLED, LOW);
-//    flash_time = millis();
-//    message_sent = false;
-//  }
+  // test BLED
+  bool led_on = millis() - flash_time >= 1000;
+  bool reset_led = millis() - flash_time >= 2000;
+  // if 1 second has passed and light is currently off
+  if (led_on)
+  {
+    digitalWrite(BLED, HIGH);
+    if (!message_sent)
+    {
+      //Serial.println("BLED on");
+      message_sent = true;
+    }
+  }
+  // if less than 1 second and light is on
+  if (reset_led)
+  {
+    digitalWrite(BLED, LOW);
+    flash_time = millis();
+    message_sent = false;
+  }
 
-
-  Serial.print(digitalRead(zero_switch));
+  Serial.print(analogRead(FLEX1));
   Serial.print("\t");
-  Serial.println(digitalRead(BLED));
+  Serial.print(analogRead(FLEX2));
+  Serial.print("\t");
+  Serial.print(analogRead(FLEX3));
+  Serial.print("\t");
+  Serial.print(analogRead(FLEX4));
+  Serial.println();
+
+
+//  Serial.print(digitalRead(zero_switch));
+//  Serial.print("\t");
+//  Serial.println(digitalRead(BLED));
   
 
-  delay(1000);
+  delay(10);
 
 }
